b035e35d73113cdc4241f1b013f8a54770d1d3a8
NickW16 <171855220+NickW16@users.noreply.github.com>
2025-07-01 11:13:28 -0300

    First commit, create hash function

---
+++ index.js
@@ -0,0 +1,13 @@
+function HashMap (loadFactor, capacity) {
+
+    function hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 10000;
+        }
+
+        return hashCode;
+    }
+}
